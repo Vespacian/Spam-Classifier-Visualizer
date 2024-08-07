226b389a550b5dad8e52da235fa74be287fa883c
Eric Bae <eric.kh.bae@gmail.com>
2024-08-06 21:51:12 -0700

    experimenting with components

--- ClassificationTree.java
+++ ClassificationTree.java
@@ -3,133 +3,28 @@ import java.io.*;
 
 public class ClassificationTree extends Classifier {
     // TODO: Implement these!
-    private ClassificationNode overallRoot;
     
-    // throws IAE
-    public ClassificationTree(List<Classifiable> data, List<String> results) {
-        if (data.size() == 0 || results.size() == 0 || data.size() != results.size()) {
-            throw new IllegalArgumentException("input lists not good nono");
-        }
-        
-        for (int i = 0; i < data.size(); i++) {
-            overallRoot = trainTree(data.get(i), results.get(i), overallRoot);
-        }
+    public ClassificationTree(List<Classifiable> data, List<String> labels) {
+        throw new UnsupportedOperationException("Not yet implemented!");
     }
 
-    private ClassificationNode trainTree(Classifiable data, String label, ClassificationNode node) {
-        if (node == null) {
-            return new ClassificationNode(label, data);
-        } else if (node.label != null && node.label != label) {
-            ClassificationNode left = new ClassificationNode(label, data);
-            ClassificationNode right = new ClassificationNode(node.label, node.dataPoint);
-            
-            Split split = node.dataPoint.partition(data);
-            if (split.evaluate(data)) {
-                ClassificationNode temp = left;
-                left = right;
-                right = temp;
-            }
-            return new ClassificationNode(node.dataPoint.partition(data), left, right);
-        } else {
-            return (node.split.evaluate(data)) ? trainTree(data, label, node.right) : trainTree(data, label, node.left);
-        }  
-    }
-
-    // throws ISE
     public ClassificationTree(Scanner sc) {
-        if (!sc.hasNextLine()) {
-            throw new IllegalStateException("blah blah blah No file to use - empty tree made blah blah blah");
-        }
-        overallRoot = buildTree(sc);
-    }
-
-    // completely assumes that the file is in the correct format
-    private ClassificationNode buildTree(Scanner sc) {
-        String line = sc.nextLine();
-        // only two types of nodes possible: 
-        if (line.startsWith("Feature:")) {
-            sc.next(); // get rid of "Threshold: "
-            double threshold = sc.nextDouble();
-            return new ClassificationNode(new Split(line, threshold), buildTree(sc), buildTree(sc));
-        } else {
-            return new ClassificationNode(line); // base case
-        }
+        throw new UnsupportedOperationException("Not yet implemented!");
     }
     
     public boolean canClassify(Classifiable input) {
-        Set<String> features = input.getFeatures();
-        return canClassify(features, overallRoot);
+        throw new UnsupportedOperationException("Not yet implemented!");
     }
 
-    private boolean canClassify(Set<String> features, ClassificationNode node) {
-        if (node.label == null) {
-            return features.contains(node.split.getFeature()) 
-                && canClassify(features, node.left) 
-                && canClassify(features, node.right);
-        }
-        return true;
-    }
-
-    // throws IAE
     public String classify(Classifiable input) {
-        if (!canClassify(input)) {
-            throw new IllegalArgumentException("Unable to classify the given input. The square block doesn't fit in the triangle hole :(");
-        }
-        return classify(input, overallRoot);
-    }
-
-    private String classify(Classifiable input, ClassificationNode node) {
-        if (node.label != null) {
-            return node.label;
-        } else {
-            return (node.split.evaluate(input)) ? classify(input, node.right) : classify(input, node.left);
-        }        
+        throw new UnsupportedOperationException("Not yet implemented!");
     }
 
     public void save(PrintStream ps) {
-        save(ps, overallRoot);
-    }
-
-    private void save(PrintStream ps, ClassificationNode node) {
-        if (node.label != null) {
-            ps.println(node.label);
-        } else {
-            ps.println(node.split.toString());
-            save(ps, node.left);
-            save(ps, node.right);
-        }
+        throw new UnsupportedOperationException("Not yet implemented!");
     }
 
     public static class ClassificationNode {
-        // data
-        public String label;
-        public Split split; // ig it's also possible to not use this datatype and have all the fields in Split here instead - I think this should be a deduction
-        public Classifiable dataPoint;
-        // references
-        private ClassificationNode left; // deduct on morphing
-        private ClassificationNode right;
-
-        // constructors
-        // label node guarantees that it is a leaf node - will use label != null to show that we're currently at leaf node
-        public ClassificationNode(String label) {
-            this (null, null, null);
-            this.label = label;
-        }
-
-        public ClassificationNode(String label, Classifiable dataPoint) {
-            this (label);
-            this.dataPoint = dataPoint;
-        }
-
-        public ClassificationNode(Split split) {
-            this (split, null, null);
-        }
-
-        public ClassificationNode(Split split, ClassificationNode left, ClassificationNode right) {
-            this.split = split;
-            this.left = left;
-            this.right = right;
-        }
-
+        // TODO: Design this!
     }
 }
--- Pilot.java
+++ Pilot.java
@@ -1,16 +1,25 @@
+
+// java libraries
+import javax.swing.JFrame;
+import java.util.Hashtable;
+import java.util.Map;
+
+// Datastructures
 import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
-
-// import com.mxgraph.layout.mxCircleLayout;
-import com.mxgraph.layout.mxCompactTreeLayout;
-import com.mxgraph.swing.mxGraphComponent;
-
-import javax.swing.JFrame;
-
 import org.jgrapht.Graph;
 import org.jgrapht.ext.JGraphXAdapter;
 
-public class Pilot extends JFrame{
+// Visualization
+import com.mxgraph.layout.mxCompactTreeLayout;
+import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.view.mxGraph;
+import com.mxgraph.view.mxStylesheet;
+import com.mxgraph.util.mxConstants;
+import com.mxgraph.util.mxPoint;
+
+
+public class Pilot extends JFrame {
 
     public static void main(String[] args) {
         Pilot frame = new Pilot();
@@ -20,7 +29,7 @@ public class Pilot extends JFrame{
     }
 
     public Pilot() {
-// Create a binary tree graph
+        // Create a binary tree graph
         Graph<String, DefaultEdge> binaryTree = new DefaultDirectedGraph<>(DefaultEdge.class);
 
         // Add vertices (nodes)
@@ -42,12 +51,68 @@ public class Pilot extends JFrame{
 
         // Create a visualization using JGraphX
         JGraphXAdapter<String, DefaultEdge> graphAdapter = new JGraphXAdapter<>(binaryTree);
+
+        // Customize the graph appearance
+        customizeGraphStyles(graphAdapter);
+
         mxGraphComponent graphComponent = new mxGraphComponent(graphAdapter);
+        graphComponent.getGraph().getView().setTranslate(new mxPoint(50, 50));
         getContentPane().add(graphComponent);
 
         // Layout the graph as a tree
         mxCompactTreeLayout layout = new mxCompactTreeLayout(graphAdapter);
         layout.setHorizontal(false);  // Set to false for a vertical tree layout, true for horizontal
+        layout.setNodeDistance(30);   // Set the distance between nodes
+        layout.setLevelDistance(50);  // Set the distance between levels
         layout.execute(graphAdapter.getDefaultParent());
     }
+
+    private void customizeGraphStyles(mxGraph graph) {
+        // Define the styles for nodes and edges
+        mxStylesheet stylesheet = graph.getStylesheet();
+        Map<String, Object> nodeStyle = new Hashtable<>();
+        
+        // node styling
+        nodeStyle.put(mxConstants.STYLE_SHAPE, mxConstants.ARROW_OVAL);
+        nodeStyle.put(mxConstants.STYLE_FONTCOLOR, "#000000");
+        nodeStyle.put(mxConstants.STYLE_FILLCOLOR, "#87CEEB");
+        nodeStyle.put(mxConstants.STYLE_STROKECOLOR, "#000000");
+        nodeStyle.put(mxConstants.STYLE_FONTSIZE, 16);
+
+        // node spacing
+        nodeStyle.put(mxConstants.STYLE_AUTOSIZE, true);
+        nodeStyle.put(mxConstants.STYLE_SPACING, 10);
+
+        // node moveable
+        nodeStyle.put(mxConstants.STYLE_MOVABLE, true);
+        nodeStyle.put(mxConstants.STYLE_RESIZABLE, false);
+        nodeStyle.put(mxConstants.STYLE_EDITABLE, false);
+
+
+        // nodeStyle.put(mxConstants.STYLE_WHITE_SPACE, "wrap"); // Enable text wrapping
+        // nodeStyle.put(mxConstants.STYLE_VERTICAL_ALIGN, mxConstants.ALIGN_MIDDLE);
+        // nodeStyle.put(mxConstants.STYLE_ALIGN, mxConstants.ALIGN_CENTER);
+
+
+        stylesheet.putCellStyle("NODE", nodeStyle);
+
+        Map<String, Object> edgeStyle = new Hashtable<>();
+        edgeStyle.put(mxConstants.STYLE_STROKECOLOR, "#000000");
+        edgeStyle.put(mxConstants.STYLE_NOLABEL, true);
+        stylesheet.putCellStyle("EDGE", edgeStyle);
+
+        // Apply styles to all vertices and edges
+        Object[] cells = graph.getChildCells(graph.getDefaultParent(), true, true);
+        for (Object cell : cells) {
+            if (graph.getModel().isVertex(cell)) {
+                graph.setCellStyle("NODE", new Object[]{cell});
+            } else if (graph.getModel().isEdge(cell)) {
+                graph.setCellStyle("EDGE", new Object[]{cell});
+            }
+        }
+    }
 }
+
+
+
+
